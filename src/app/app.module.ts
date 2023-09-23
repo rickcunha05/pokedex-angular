1f18d06c15dfe5cddd489c4cbec0b4f9a3a826ea
rickcunha05 <henrique.cunha5@outlook.com>
2023-09-23 13:30:09 -0300

    projeto criado para aprender sobre angular.

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { PokemonService } from './services/pokemon.service';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonListComponent,
+    PokemonCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule   
+  ],
+  providers: [PokemonService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.sass']
+})
+export class PokemonListComponent {
+
+  constructor(public PokemonService: PokemonService) {
+  }
+
+}
